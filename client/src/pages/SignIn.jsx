8c3607c6ce68acbbd21b721a9bec9ac35eb39586
Brian Njuguna <brian@ibuqacapital.com>
2024-01-20 01:18:12 +0300

    user sign up ui and auth

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">SignIn</h1>
+    </div>
+  );
 }
